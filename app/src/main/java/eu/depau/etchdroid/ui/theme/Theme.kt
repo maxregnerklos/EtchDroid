1316df6e1e29db16ce3059fbb3927b5e549cd1b1
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2023-06-04 02:13:26 -0400

    Update Theme.kt

--- app/src/main/java/eu/depau/etchdroid/ui/theme/Theme.kt
+++ app/src/main/java/eu/depau/etchdroid/ui/theme/Theme.kt
@@ -1,127 +1,51 @@
-package eu.depau.etchdroid.ui.theme
+package com.example.app.ui
 
-import android.app.Activity
-import android.os.Build
+import android.content.Context
+import android.content.Intent
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.net.Uri
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import com.example.app.AppSettings
+import com.example.app.Intents
+import com.example.app.JobStatusInfo
+import com.example.app.SettingChangeListener
+import com.example.app.ThemeMode
+import com.example.app.massstorage.EtchDroidUsbMassStorageDevice.Companion.massStorageDevices
+import com.example.app.massstorage.UsbMassStorageDeviceDescriptor
+import com.example.app.utils.exception.base.EtchDroidException
+import com.example.app.utils.exception.base.RecoverableException
+import com.example.app.utils.ktexts.safeParcelableExtra
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 
-val LightColorScheme = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    secondaryContainer = md_theme_light_secondaryContainer,
-    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-    tertiary = md_theme_light_tertiary,
-    onTertiary = md_theme_light_onTertiary,
-    tertiaryContainer = md_theme_light_tertiaryContainer,
-    onTertiaryContainer = md_theme_light_onTertiaryContainer,
-    error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
-    onError = md_theme_light_onError,
-    onErrorContainer = md_theme_light_onErrorContainer,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-    surfaceVariant = md_theme_light_surfaceVariant,
-    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
-    inverseSurface = md_theme_light_inverseSurface,
-    inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
-    outlineVariant = md_theme_light_outlineVariant,
-    scrim = md_theme_light_scrim,
-)
-
-
-val DarkColorScheme = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
-)
-
-data class EtchDroidThemeScope(
-    val darkTheme: Boolean,
-    val dynamicColor: Boolean
-)
-
-@Composable
-fun EtchDroidTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
-    content: @Composable EtchDroidThemeScope.() -> Unit
-) {
-//    val systemUiController = rememberSystemUiController()
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        val currentWindow = (view.context as? Activity)?.window
-            ?: throw Exception("Not in an activity - unable to get Window reference")
-        SideEffect {
-            currentWindow.apply {
-                statusBarColor = colorScheme.surface.toArgb()
-                navigationBarColor = colorScheme.surface.toArgb()
-            }
-            WindowCompat.getInsetsController(currentWindow, view).apply {
-                isAppearanceLightStatusBars = !darkTheme
-                isAppearanceLightNavigationBars = !darkTheme
-            }
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = {
-            EtchDroidThemeScope(
-                darkTheme,
-                dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-            ).content()
-        }
-    )
+interface IThemeState {
+    val dynamicColors: Boolean
+    val themeMode: ThemeMode
 }
+
+interface IViewModel<T> {
+    val state: StateFlow<T>
+}
+
+interface ThemeViewModel<T> : IViewModel<T> where T : IThemeState {
+    val darkMode: State<Boolean>
+        @Composable get() {
+            val stateValue by state.collectAsState()
+            val systemInDarkMode = isSystemInDarkTheme()
+            return remember(stateValue.themeMode) {
+                derivedStateOf {
+                    when (stateValue.themeMode) {
+                        ThemeMode.SYSTEM -> systemInDarkMode
+                        ThemeMode.DARK -> true
+                        ThemeMode
